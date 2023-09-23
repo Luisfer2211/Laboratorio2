f13267dfe489934d09d1f37a77a3a507a1baf372
Luisfer-2211 <128609084+Luisfer-2211@users.noreply.github.com>
2023-09-22 23:58:39 -0600

    Update CargaArchivoCSV.java

--- CargaArchivoCSV.java
+++ CargaArchivoCSV.java
@@ -1,42 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Salon> salones = CargaArchivoCSV.cargarSalonesDesdeCSV("salones.csv");
-        List<Curso> cursos = CargaArchivoCSV.cargarCursosDesdeCSV("cursos.csv");
-        AsignacionSalones asignacionSalones = new AsignacionSalones(cursos, salones);
-
-        while (true) {
-            System.out.println("Menú:");
-            System.out.println("1. Asignar Salones a Cursos");
-            System.out.println("2. Generar Informe");
-            System.out.println("3. Salir");
-            System.out.print("Ingrese el número de opción: ");
-            int opcion = scanner.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    asignacionSalones.asignarSalones();
-                    System.out.println("Asignación de salones completada.");
-                    break;
-                case 2:
-                    List<Curso> cursosAsignados = asignacionSalones.getCursosAsignados();
-                    List<Curso> cursosSinSalon = asignacionSalones.getCursosSinSalon();
-                    Informe informe = new Informe(cursosAsignados, cursosSinSalon);
-                    informe.generarInforme("informe.txt");
-                    System.out.println("Informe generado con éxito.");
-                    break;
-                case 3:
-                    System.out.println("Saliendo del programa.");
-                    scanner.close();
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor, ingrese un número válido.");
-                    break;
+public class CargaArchivoCSV {
+    public static List<Salon> cargarSalonesDesdeCSV(String archivo) {
+        List<Salon> salones = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(",");
+                if (partes.length == 5) {
+                    int idSede = Integer.parseInt(partes[0]);
+                    char edificio = partes[1].charAt(0);
+                    int nivel = Integer.parseInt(partes[2]);
+                    int idSalon = Integer.parseInt(partes[3]);
+                    int capacidad = Integer.parseInt(partes[4]);
+                    Salon salon = new Salon(idSede, edificio, nivel, idSalon, capacidad);
+                    salones.add(salon);
+                } else {
+                    System.out.println("Error: línea incorrecta en el archivo CSV de salones.");
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return salones;
+    }
+
+    public static List<Curso> cargarCursosDesdeCSV(String archivo) {
+        List<Curso> cursos = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(",");
+                if (partes.length == 7) { 
+                    int idCurso = Integer.parseInt(partes[0]);
+                    int idSede = Integer.parseInt(partes[1]);
+                    String nombreCurso = partes[2];
+                    int horario = Integer.parseInt(partes[3]);
+                    int duracion = Integer.parseInt(partes[4]);
+                    String[] dias = partes[5].split(",");
+                    int cantidadEstudiantes = Integer.parseInt(partes[6]);
+                    Curso curso = new Curso(idCurso, idSede, nombreCurso, horario, duracion, dias, cantidadEstudiantes);
+                    cursos.add(curso);
+                } else {
+                    System.out.println("Error: línea incorrecta en el archivo CSV de cursos.");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cursos;
     }
 }
